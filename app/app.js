a67f393e5b388043180aadf740b9d18802e86b99
jjontteok <nayeong0121@gmail.com>
2025-04-07 01:51:43 +0900

    프런트 단 javascript 파일만들기

--- app/app.js
+++ app/app.js
@@ -11,6 +11,10 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+//__dirname : app.js가 있는 위치를 반환함 -> 현재 파일의 디렉토리 기준으로 src/public 경로 지정
+//src/public 폴더를 정적 파일 경로로 지정해서, 브라우저가 직접 접근할 수 있도록 함
+app.use(express.static(`${__dirname}/src/public`));
+
 app.use("/", home);
 
 module.exports = app;
