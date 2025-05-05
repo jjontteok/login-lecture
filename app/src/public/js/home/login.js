4dbc70b96c483b863334ad91ee0cc27cc243153c
jjontteok <nayeong0121@gmail.com>
2025-05-05 17:17:58 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -32,5 +32,14 @@ function login() {
     body: JSON.stringify(req),
   })
     .then((res) => res.json())
-    .then((res) => console.log(res));
+    .then((res) => {
+      if (res.success) {
+        location.href = "/"; //루트로 간다.
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error(new Error("로그인 중 에러 발생"));
+    });
 }
