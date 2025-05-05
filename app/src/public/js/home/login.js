a83b4608038c38ef337936c7ebeeec8fd0052930
jjontteok <nayeong0121@gmail.com>
2025-05-05 15:21:07 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+//querySelector
+//id를 불러올 때 #을 붙이는 것 <input id = "psword"../> 에서 psword를 불러와달라는 의미
+const id = document.querySelector("#id"), //login.ejs의 id로 불러온다.
+  psword = document.querySelector("#psword"), //login.ejs의 id로 불러온다.
+  loginBtn = document.querySelector("button"); //버튼은 하나이기 때문에 button태그로 불러온다.
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+  console.log(req);
+}
